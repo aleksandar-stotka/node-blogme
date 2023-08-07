ebbf83bfc63885a87be5e01bd78d2353f9c6087d
Aleksandar Angelevski <aangelevski6@gmail.com>
2023-08-06 17:32:38 -0700

    set-create-page

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ app.get('/about', (req,res) => {
     res.render('about')
 })
 
-
-app.get('/about-us', (req,res) => {
-    res.redirect('/about')
+app.get('/blogs/create', (req,res) => {
+    res.render('create')
 })
+
 
 app.use((req,res) => {
     res.status(404).render('404')
